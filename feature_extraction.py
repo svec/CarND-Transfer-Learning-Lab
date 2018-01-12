8022c3938448a7f8a86851bdc570f8cb9250ac91
Christopher Svec <csvec@irobot.com>
2018-01-11 21:05:33 -0500

    Finished feature_extraction.py, it works on all 3 networks.

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,8 @@
 import pickle
 import tensorflow as tf
-# TODO: import Keras layers you need here
+import numpy as np
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -8,7 +10,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
-
+flags.DEFINE_integer('epochs', 50, "# epochs")
+flags.DEFINE_integer('batch_size', 256, "batch size")
 
 def load_bottleneck_data(training_file, validation_file):
     """
@@ -38,16 +41,26 @@ def main(_):
     # load bottleneck data
     X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
 
-    print(X_train.shape, y_train.shape)
-    print(X_val.shape, y_val.shape)
+    print("X_train, y_train shapes:", X_train.shape, y_train.shape)
+    print("X_val, y_val shapes:    ", X_val.shape, y_val.shape)
+
+    num_classes = len(np.unique(y_train))
+    print("num_classes:", num_classes)
 
     # TODO: define your model and hyperparams here
     # make sure to adjust the number of classes based on
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    input_shape = X_train.shape[1:]
+    input_layer = Input(shape=input_shape)
+    x = Flatten()(input_layer)
+    x = Dense(num_classes, activation='softmax')(x)
+    model = Model(input_layer, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     # TODO: train your model here
+    model.fit(X_train, y_train, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size, validation_data=(X_val, y_val), shuffle=True)
 
 
 # parses flags and calls the `main` function above
